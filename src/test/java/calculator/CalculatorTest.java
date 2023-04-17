310c5979041b57bb3806a107a4cbb76f319c4af3
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-04-17 15:36:05 +0200

    Initial commit

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import calculator.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    private Calculator sumCalculator;
+
+    @BeforeEach
+    void setUp() {
+        sumCalculator = new Calculator();
+    }
+
+    @Test
+    void sumOne() {
+        assertEquals(1, sumCalculator.sum(1));
+    }
+    @Test
+    void sumThree() {
+        assertEquals(6, sumCalculator.sum(3));
+    }
+
+    @Test
+    void sumZero() {
+        assertThrows(IllegalArgumentException.class, () -> sumCalculator.sum(0));
+    }
+}
